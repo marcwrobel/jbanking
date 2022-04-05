c09ab8ae4b32c98f4694bbc2f3d9379a7a52a3c2
TheYingThing <yingling.dang@gmail.com>
2022-04-05 23:59:35 +0200

    added visa card check

--- src/test/java/fr/marcwrobel/jbanking/creditcard/CreditCardTest.java
+++ src/test/java/fr/marcwrobel/jbanking/creditcard/CreditCardTest.java
@@ -10,10 +10,17 @@ import org.junit.jupiter.api.Test;
 public class CreditCardTest {
 
   @Test
-  @DisplayName("Return valid card type")
-  void returnsType() {
+  @DisplayName("Return valid card type (MASTERCARD)")
+  void returnsTypeMaster() {
     CreditCard card = new CreditCard(MASTERCARD, "5412345678912345");
     assertEquals("Mastercard", card.getType()) ;
+  }
+
+  @Test
+  @DisplayName("Return valid card type (VISA)")
+  void returnsTypeVisa() {
+    CreditCard card = new CreditCard(VISA, "5412345678912345");
+    assertEquals("Visa", card.getType()) ;
   }
 
   @Test
