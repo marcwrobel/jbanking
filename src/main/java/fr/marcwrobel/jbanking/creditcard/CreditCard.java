b77f7733bf9a34908139364b2192c93834b5dd7c
rebeccaBraun <rebaccabraun6@gmail.com>
2022-04-05 23:08:33 +0200

    complete checkCreditCardNumber

--- src/main/java/fr/marcwrobel/jbanking/creditcard/CreditCard.java
+++ src/main/java/fr/marcwrobel/jbanking/creditcard/CreditCard.java
@@ -1,31 +1,32 @@
 package fr.marcwrobel.jbanking.creditcard;
 
 public class CreditCard {
-    private CreditCardType type;
-    private String cardnumber;
+  private CreditCardType type;
+  private String cardnumber;
 
-    private enum CreditCardType {
-      MASTERCARD,
-      VISA
-    }
+  private enum CreditCardType {
+    MASTERCARD,
+    VISA
+  }
 
-    public CreditCard(CreditCardType type, String cardnumber) {
-        this.cardnumber = cardnumber;
-        this.type = type;
-    }
+  public CreditCard(CreditCardType type, String cardnumber) {
+    this.cardnumber = cardnumber;
+    this.type = type;
+  }
 
-    public String getType() {
-      switch (type) {
-        case MASTERCARD:
-          return "Mastercard";
-        case VISA:
-          return "Visa";
-        default:
-          return "None";
-      }
+  public String getType() {
+    switch (type) {
+      case MASTERCARD:
+        return "Mastercard";
+      case VISA:
+        return "Visa";
+      default:
+        return "None";
     }
+  }
 
-    public String getCardnumber() {
-      return this.cardnumber;
-    }
+  public String getCardnumber() {
+    return this.cardnumber;
+  }
 }
+
